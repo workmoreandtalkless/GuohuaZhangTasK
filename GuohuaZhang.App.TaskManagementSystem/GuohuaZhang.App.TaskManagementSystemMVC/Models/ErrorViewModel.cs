390d858384bd162fd4f9a9beabb41a61d9ee9d65
guohua zhang <guohuaromney@hotmail.com>
2021-07-26 15:10:23 -0400

    initial project

---
+++ GuohuaZhang.App.TaskManagementSystem/GuohuaZhang.App.TaskManagementSystemMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GuohuaZhang.App.TaskManagementSystemMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ GuohuaZhang.App.TaskManagementSystem/Infrastructure/Repositories/TasksRepository.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class TasksRepository :  ITasksRepository
+    {
+        
+    }
+}
